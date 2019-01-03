92bdfd9e51600855ab944ee31306a9070f6d789b
MarsMan13 <choigeon0513@gmail.com>
2019-01-03 13:05:45 +0900

    day 5

---
+++ js/js8.js
@@ -0,0 +1,14 @@
+//window_object
+
+open("https://www.naver.com", "NAVER", "width=350, height=400, left = 50, top = 10; scrollbars = no, status = no, location = no");
+
+window.alert("!!!");
+
+var t1 = prompt("What is your name?", "");
+document.write(t1, "<br>");
+//
+
+var t2 = confirm("Let's go!!");
+document.write(t2, "<br>");
+
+//
