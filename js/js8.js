f066b4461cde3072d3a9b543a876780f6e5dbdf0
MarsMan13 <choigeon0513@gmail.com>
2019-01-04 19:12:29 +0900

    day 5

--- js/js8.js
+++ js/js8.js
@@ -1,6 +1,9 @@
 //window_object
 
-open("https://www.naver.com", "NAVER", "width=350, height=400, left = 50, top = 10; scrollbars = no, status = no, location = no");
+//open("https://www.naver.com", "NAVER", "width=350, height=400, left = 50, top = 10; scrollbars = no, status = no, location = no");
+
+window.open("windowPopUp.html", "pop1", "width=300, height=300, \
+left=300, top = 30, toolbars= yes");
 
 window.alert("!!!");
 
@@ -12,3 +15,4 @@ var t2 = confirm("Let's go!!");
 document.write(t2, "<br>");
 
 //
+window.moveTo(100, 200);
