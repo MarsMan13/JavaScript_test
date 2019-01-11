bbc670be4e0e8286685c8a1d04a1d25ab5d51917
MarsMan13 <choigeon0513@gmail.com>
2019-01-11 23:18:46 +0900

    day 8

---
+++ js/theFunctionAsValue.js
@@ -0,0 +1,28 @@
+(function(){
+    function cal(mode){
+        var funcs = {
+            'plus': function(left, right){return left+right},
+            'minus': function(left, right){return left-right}
+        }
+        
+        return funcs[mode];
+    }
+    
+    alert(cal("plus")(2, 1));
+    alert(cal("minus")(2, 1));
+    
+}())
+
+(function(){
+    var ary = [
+        function(a, b){return a+b},
+        function(a, b){return a-b},
+        function(a, b){return a*b},
+    ];
+    
+    for(var i = 0; i<3; i++){
+        input = ary[i](2,3);
+        alert(input);
+    }
+    
+}())
