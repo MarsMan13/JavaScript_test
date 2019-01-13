20374e11bc013e08d93acfb27c295a6f4d5b8453
MarsMan13 <choigeon0513@gmail.com>
2019-01-13 23:22:43 +0900

    day 9

---
+++ js/closure.js
@@ -0,0 +1,12 @@
+var arr = {};
+for(var i = 0 ; i< 5; i++){
+    arr[i] = function(id){
+        return function(){ return id };
+    }(i);
+}
+
+console.log('start');
+
+for(var index in arr){
+    console.log(arr[index]());
+}
